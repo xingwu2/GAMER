b0b9fb7ac3e6c5e2fcf5e439988f238999c4cf30
xingwu2 <wuxingtom@gmail.com>
2025-12-29 03:58:54 -0800

    small fix

--- python/multiplicative_gibbs.py
+++ python/multiplicative_gibbs.py
@@ -187,187 +187,187 @@ def sample_beta(y,C_alpha,H,beta,gamma,sigma_1,sigma_e,H_beta):
 	return(beta,H_beta)
 
 
-def sampling(verbose,y,C,HapDM,iters,prefix,num,trace_container,gamma_container,beta_container,alpha_container,pi_b):
+# def sampling(verbose,y,C,HapDM,iters,prefix,num,trace_container,gamma_container,beta_container,alpha_container,pi_b):
 
-	## set random seed for the process
-	np.random.seed(int(time.time()) + os.getpid())
+# 	## set random seed for the process
+# 	np.random.seed(int(time.time()) + os.getpid())
 
-	#initiate beta,gamma and H matrix
-	C_r, C_c = C.shape
+# 	#initiate beta,gamma and H matrix
+# 	C_r, C_c = C.shape
 
-	H = np.array(HapDM)
-	H = np.asfortranarray(H)
+# 	H = np.array(HapDM)
+# 	H = np.asfortranarray(H)
 
 
-	H_r,H_c = H.shape
+# 	H_r,H_c = H.shape
 
-	##specify hyper parameters
-	pie_a = 1
-	pie_b = H_c*pi_b
-	print(pie_b)
-	# if pie_b < 10000:
-	# 	pie_b = 10000
+# 	##specify hyper parameters
+# 	pie_a = 1
+# 	pie_b = H_c*pi_b
+# 	print(pie_b)
+# 	# if pie_b < 10000:
+# 	# 	pie_b = 10000
 	
-	a_sigma = 1
-	b_sigma = 1
-	a_e = 1
-	b_e = 1
+# 	a_sigma = 1
+# 	b_sigma = 1
+# 	a_e = 1
+# 	b_e = 1
 
-	sigma_1 = math.sqrt(1/np.random.gamma(a_sigma,b_sigma))
-	sigma_e = math.sqrt(1/np.random.gamma(a_e,b_e))
-	pie = np.random.beta(pie_a,pie_b)
+# 	sigma_1 = math.sqrt(1/np.random.gamma(a_sigma,b_sigma))
+# 	sigma_e = math.sqrt(1/np.random.gamma(a_e,b_e))
+# 	pie = np.random.beta(pie_a,pie_b)
 	
-	print("parameter initiation:",sigma_1,sigma_e,pie)
+# 	print("parameter initiation:",sigma_1,sigma_e,pie)
 
-	#initiate alpha, alpha_trace, beta_trace and gamma_trace
+# 	#initiate alpha, alpha_trace, beta_trace and gamma_trace
 
-	it = 0
-	burn_in_iter = 2000
-	trace = np.empty((iters-2000,5))
-	alpha_trace = np.empty((iters-2000,C_c))
-	gamma_trace = np.empty((iters-2000,H_c))
-	beta_trace = np.empty((iters-2000,H_c))
-	top5_beta_trace = np.empty((iters-2000,5))
+# 	it = 0
+# 	burn_in_iter = 2000
+# 	trace = np.empty((iters-2000,5))
+# 	alpha_trace = np.empty((iters-2000,C_c))
+# 	gamma_trace = np.empty((iters-2000,H_c))
+# 	beta_trace = np.empty((iters-2000,H_c))
+# 	top5_beta_trace = np.empty((iters-2000,5))
 
-	alpha = np.random.random(size = C_c)
-	gamma = np.random.binomial(1,pie,H_c)
-	#beta = np.array(np.zeros(H_c))
-	beta = np.array(np.zeros(H_c))
+# 	alpha = np.random.random(size = C_c)
+# 	gamma = np.random.binomial(1,pie,H_c)
+# 	#beta = np.array(np.zeros(H_c))
+# 	beta = np.array(np.zeros(H_c))
 	
-	for i in range(H_c):
-		if gamma[i] == 0:
-			beta[i] = 0
-		else:
-			beta[i] = np.random.normal(0,sigma_1) 
+# 	for i in range(H_c):
+# 		if gamma[i] == 0:
+# 			beta[i] = 0
+# 		else:
+# 			beta[i] = np.random.normal(0,sigma_1) 
 
-	## Pre-compute the H_beta and C_alpha in the beginning, and update them later in the MCMC
-	H_beta = circle_product_matrix(H,beta)
-	C_alpha = np.matmul(C,alpha)
+# 	## Pre-compute the H_beta and C_alpha in the beginning, and update them later in the MCMC
+# 	H_beta = circle_product_matrix(H,beta)
+# 	C_alpha = np.matmul(C,alpha)
 
-	C_norm_2 = np.sum(C**2,axis=0)
-	H_norm_2 = np.sum(H**2,axis=0)
+# 	C_norm_2 = np.sum(C**2,axis=0)
+# 	H_norm_2 = np.sum(H**2,axis=0)
 
-	#start sampling
+# 	#start sampling
 
-	while it < iters:
+# 	while it < iters:
 	
-		before = time.time()
-		sigma_1 = sample_sigma_1(beta,gamma,a_sigma,b_sigma)
-		if sigma_1 < 0.05:
-			sigma_1 = 0.05
-		pie = sample_pie(gamma,pie_a,pie_b)
-		sigma_e = sample_sigma_e(y,H_beta,C_alpha,a_e,b_e)
-		gamma = sample_gamma_numba(y,C_alpha,H,beta,pie,sigma_1,sigma_e,gamma,H_beta)
-		#gamma = sample_gamma(y,C_alpha,H,beta,pie,sigma_1,sigma_e,gamma,H_beta)
-		alpha,C_alpha = sample_alpha(y,C,alpha,sigma_e,H_beta,C_alpha)
-		#beta,H_beta = sample_beta(y,C_alpha,H,beta,gamma,sigma_1,sigma_e,H_beta)
-		beta,H_beta = sample_beta_numba(y,C_alpha,H,beta,gamma,sigma_1,sigma_e,H_beta)
-		genetic_var = np.var(H_beta)
-		pheno_var = np.var(y - C_alpha)
-		large_beta_ratio = np.sum(np.absolute(beta) > 0.3) / len(beta)
-		total_heritability = genetic_var / pheno_var
-		after = time.time()
-		if it > 5000 and (total_heritability > 1):
-			print("unrealistic beta sample",it,genetic_var,pheno_var,total_heritability)
-			continue
+# 		before = time.time()
+# 		sigma_1 = sample_sigma_1(beta,gamma,a_sigma,b_sigma)
+# 		if sigma_1 < 0.05:
+# 			sigma_1 = 0.05
+# 		pie = sample_pie(gamma,pie_a,pie_b)
+# 		sigma_e = sample_sigma_e(y,H_beta,C_alpha,a_e,b_e)
+# 		gamma = sample_gamma_numba(y,C_alpha,H,beta,pie,sigma_1,sigma_e,gamma,H_beta)
+# 		#gamma = sample_gamma(y,C_alpha,H,beta,pie,sigma_1,sigma_e,gamma,H_beta)
+# 		alpha,C_alpha = sample_alpha(y,C,alpha,sigma_e,H_beta,C_alpha)
+# 		#beta,H_beta = sample_beta(y,C_alpha,H,beta,gamma,sigma_1,sigma_e,H_beta)
+# 		beta,H_beta = sample_beta_numba(y,C_alpha,H,beta,gamma,sigma_1,sigma_e,H_beta)
+# 		genetic_var = np.var(H_beta)
+# 		pheno_var = np.var(y - C_alpha)
+# 		large_beta_ratio = np.sum(np.absolute(beta) > 0.3) / len(beta)
+# 		total_heritability = genetic_var / pheno_var
+# 		after = time.time()
+# 		if it > 5000 and (total_heritability > 1):
+# 			print("unrealistic beta sample",it,genetic_var,pheno_var,total_heritability)
+# 			continue
 
-		else:
-			if verbose:
-				print(it,str(after - before),pie,sigma_1,sigma_e,sum(gamma),large_beta_ratio,max(abs(beta)),total_heritability)
+# 		else:
+# 			if verbose:
+# 				print(it,str(after - before),pie,sigma_1,sigma_e,sum(gamma),large_beta_ratio,max(abs(beta)),total_heritability)
 
-			if it >= burn_in_iter:
-				trace[it-burn_in_iter,:] = [sigma_1,sigma_e,large_beta_ratio,total_heritability,sum(gamma)]
-				gamma_trace[it-burn_in_iter,:] = gamma
-				beta_trace[it-burn_in_iter,:] = beta
-				alpha_trace[it-burn_in_iter,:] = alpha
-				top5_beta_trace[it-burn_in_iter,:] = np.sort(np.absolute(beta))[::-1][:5]
+# 			if it >= burn_in_iter:
+# 				trace[it-burn_in_iter,:] = [sigma_1,sigma_e,large_beta_ratio,total_heritability,sum(gamma)]
+# 				gamma_trace[it-burn_in_iter,:] = gamma
+# 				beta_trace[it-burn_in_iter,:] = beta
+# 				alpha_trace[it-burn_in_iter,:] = alpha
+# 				top5_beta_trace[it-burn_in_iter,:] = np.sort(np.absolute(beta))[::-1][:5]
 
-			if it >= burn_in_iter + 9999: # after burn-in iterations, test convergence
+# 			if it >= burn_in_iter + 9999: # after burn-in iterations, test convergence
 
-				max_z = []
+# 				max_z = []
 				
-				for a in range(C_c):
-					after_burnin_alpha = alpha_trace[:,a]
-					alpha_zscores = geweke.geweke(after_burnin_alpha)[:,1]
-					max_z.append(np.amax(np.absolute(alpha_zscores)))
+# 				for a in range(C_c):
+# 					after_burnin_alpha = alpha_trace[:,a]
+# 					alpha_zscores = geweke.geweke(after_burnin_alpha)[:,1]
+# 					max_z.append(np.amax(np.absolute(alpha_zscores)))
 
-				for b in range(5):
-					after_burnin_beta = top5_beta_trace[:,b]
-					beta_zscores = geweke.geweke(after_burnin_beta)[:,1]
-					max_z.append(np.amax(np.absolute(beta_zscores)))
+# 				for b in range(5):
+# 					after_burnin_beta = top5_beta_trace[:,b]
+# 					beta_zscores = geweke.geweke(after_burnin_beta)[:,1]
+# 					max_z.append(np.amax(np.absolute(beta_zscores)))
 
-				#convergence for large beta ratio
-				after_burnin_pie = trace[:,2]
-				pie_zscores = geweke.geweke(after_burnin_pie)[:,1]
-				max_z.append(np.amax(np.absolute(pie_zscores)))
+# 				#convergence for large beta ratio
+# 				after_burnin_pie = trace[:,2]
+# 				pie_zscores = geweke.geweke(after_burnin_pie)[:,1]
+# 				max_z.append(np.amax(np.absolute(pie_zscores)))
 
-				#convergence for total_heritability
-				after_burnin_var = trace[:,3]
-				var_zscores = geweke.geweke(after_burnin_var)[:,1]
-				max_z.append(np.amax(np.absolute(var_zscores)))
+# 				#convergence for total_heritability
+# 				after_burnin_var = trace[:,3]
+# 				var_zscores = geweke.geweke(after_burnin_var)[:,1]
+# 				max_z.append(np.amax(np.absolute(var_zscores)))
 
-				# #convergence for sigma_1
-				# after_burnin_sigma1 = trace[:,0]
-				# sigma1_zscores = geweke.geweke(after_burnin_sigma1)[:,1]
-				# max_z.append(np.amax(np.absolute(sigma1_zscores)))
+# 				# #convergence for sigma_1
+# 				# after_burnin_sigma1 = trace[:,0]
+# 				# sigma1_zscores = geweke.geweke(after_burnin_sigma1)[:,1]
+# 				# max_z.append(np.amax(np.absolute(sigma1_zscores)))
 
-				#convergence for sigma_e
-				after_burnin_sigmae = trace[:,1]
-				sigmae_zscores = geweke.geweke(after_burnin_sigmae)[:,1]
-				max_z.append(np.amax(np.absolute(sigmae_zscores)))
+# 				#convergence for sigma_e
+# 				after_burnin_sigmae = trace[:,1]
+# 				sigmae_zscores = geweke.geweke(after_burnin_sigmae)[:,1]
+# 				max_z.append(np.amax(np.absolute(sigmae_zscores)))
 				
-				if  np.amax(max_z) < 1.5:
-					print("convergence has been reached at %i iterations." %(it))
-					break
+# 				if  np.amax(max_z) < 1.5:
+# 					print("convergence has been reached at %i iterations." %(it))
+# 					break
 
-				else:
-					trace_ = np.empty((1000,5))
-					gamma_trace_ = np.empty((1000,H_c))
-					beta_trace_ = np.empty((1000,H_c))
-					alpha_trace_ = np.empty((1000,C_c))
-					top5_beta_trace_ = np.empty((1000,5))
+# 				else:
+# 					trace_ = np.empty((1000,5))
+# 					gamma_trace_ = np.empty((1000,H_c))
+# 					beta_trace_ = np.empty((1000,H_c))
+# 					alpha_trace_ = np.empty((1000,C_c))
+# 					top5_beta_trace_ = np.empty((1000,5))
 
-					trace = np.concatenate((trace[-(iters - burn_in_iter-1000):,:],trace_),axis=0)
-					gamma_trace = np.concatenate((gamma_trace[-(iters - burn_in_iter-1000):,:],gamma_trace_),axis=0)
-					beta_trace = np.concatenate((beta_trace[-(iters - burn_in_iter-1000):,:],beta_trace_),axis=0)
-					alpha_trace = np.concatenate((alpha_trace[-(iters - burn_in_iter-1000):,:],alpha_trace_),axis=0)
-					top5_beta_trace = np.concatenate((top5_beta_trace[-(iters - burn_in_iter-1000):,:],top5_beta_trace_),axis = 0)
+# 					trace = np.concatenate((trace[-(iters - burn_in_iter-1000):,:],trace_),axis=0)
+# 					gamma_trace = np.concatenate((gamma_trace[-(iters - burn_in_iter-1000):,:],gamma_trace_),axis=0)
+# 					beta_trace = np.concatenate((beta_trace[-(iters - burn_in_iter-1000):,:],beta_trace_),axis=0)
+# 					alpha_trace = np.concatenate((alpha_trace[-(iters - burn_in_iter-1000):,:],alpha_trace_),axis=0)
+# 					top5_beta_trace = np.concatenate((top5_beta_trace[-(iters - burn_in_iter-1000):,:],top5_beta_trace_),axis = 0)
 
-					burn_in_iter += 1000
-					iters += 1000
+# 					burn_in_iter += 1000
+# 					iters += 1000
 
-			if (it - burn_in_iter) >= 0 and (it - burn_in_iter ) % 1000 == 0:
-				print("%i iterations have sampled" %(it), str(after - before),trace[it-burn_in_iter,:])
+# 			if (it - burn_in_iter) >= 0 and (it - burn_in_iter ) % 1000 == 0:
+# 				print("%i iterations have sampled" %(it), str(after - before),trace[it-burn_in_iter,:])
 
-			it += 1
+# 			it += 1
 	
-	# trace values
-	trace_container[num] = {'avg': np.mean(trace,axis=0),
-							'sd' : np.std(trace,axis=0)}
-	del trace
-	gc.collect()
+# 	# trace values
+# 	trace_container[num] = {'avg': np.mean(trace,axis=0),
+# 							'sd' : np.std(trace,axis=0)}
+# 	del trace
+# 	gc.collect()
 
-	#print("trace_container[num]",num,trace_container[num]['avg'])
-	#alpha values
-	alpha_container[num] = {'avg': np.mean(alpha_trace,axis=0),
-							'sd': np.std(alpha_trace,axis=0)}
-	del alpha_trace
-	gc.collect()
-	#print("alpha_container[num]",num,alpha_container[num]['avg'])
-	#beta values
+# 	#print("trace_container[num]",num,trace_container[num]['avg'])
+# 	#alpha values
+# 	alpha_container[num] = {'avg': np.mean(alpha_trace,axis=0),
+# 							'sd': np.std(alpha_trace,axis=0)}
+# 	del alpha_trace
+# 	gc.collect()
+# 	#print("alpha_container[num]",num,alpha_container[num]['avg'])
+# 	#beta values
 
-	beta_container[num] = {'avg':np.mean(beta_trace,axis=0),
-							'sd':np.std(beta_trace,axis=0)}
-	del beta_trace
-	gc.collect()
+# 	beta_container[num] = {'avg':np.mean(beta_trace,axis=0),
+# 							'sd':np.std(beta_trace,axis=0)}
+# 	del beta_trace
+# 	gc.collect()
 
-	gamma_container[num] = {'pip':np.mean(gamma_trace,axis=0)}
-	del gamma_trace
-	gc.collect()
+# 	gamma_container[num] = {'pip':np.mean(gamma_trace,axis=0)}
+# 	del gamma_trace
+# 	gc.collect()
 
-	# trace_container[num] = trace
-	# alpha_container[num] = alpha_trace
-	# beta_container[num] = beta_trace
-	# gamma_container[num] = gamma_trace
+# 	# trace_container[num] = trace
+# 	# alpha_container[num] = alpha_trace
+# 	# beta_container[num] = beta_trace
+# 	# gamma_container[num] = gamma_trace
 
 
